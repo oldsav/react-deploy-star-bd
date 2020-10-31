dc14ff57b34ef48f2c9428cfcb27839bcb3224c4
oldsav <oldsav@gmail.com>
2020-10-31 12:27:54 +0300

    Updates

---
+++ precache-manifest.4d156a87568745eb8f45b879327871e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad59c82656294b355b6e6ca58b6f9b0",
+    "url": "/react-deploy-star-bd/index.html"
+  },
+  {
+    "revision": "f1e6f611df310767a2c5",
+    "url": "/react-deploy-star-bd/static/css/main.0be05253.chunk.css"
+  },
+  {
+    "revision": "c33a38abf86edb1ee72a",
+    "url": "/react-deploy-star-bd/static/js/2.3dcadffc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-deploy-star-bd/static/js/2.3dcadffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e6f611df310767a2c5",
+    "url": "/react-deploy-star-bd/static/js/main.30d75238.chunk.js"
+  },
+  {
+    "revision": "e918a905ca2a139b3595",
+    "url": "/react-deploy-star-bd/static/js/runtime-main.c5f4b442.js"
+  }
+]);
